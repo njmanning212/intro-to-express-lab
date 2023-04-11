0be9d009fe1b5b160ed84d181369d72509e26b97
Nicholas Manning <njmanning212@gmail.com>
2023-04-10 19:56:03 -0500

    import students module into server.js

--- data/students.js
+++ data/students.js
@@ -1,7 +1,7 @@
 const students = [
   {text: 'Nick Manning', doneWithUnit1: true, _id: 123456},
   {text: 'Student 2', doneWithUnit1: false, _id: 654321},
-  {text: 'Student 3', doneWithUnit1: false, _id: 010101},
+  {text: 'Student 3', doneWithUnit1: false, _id: 123654},
 ]
 
 export { 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // import modules
 
 import express from 'express'
+import { students } from './data/students.js'
 
 // create Express app
 
