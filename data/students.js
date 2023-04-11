f522feffdaa8d53b22f956d947b70d4629a92eb5
Nicholas Manning <njmanning212@gmail.com>
2023-04-10 19:54:02 -0500

    create data directory and student.js module

---
+++ data/students.js
@@ -0,0 +1,9 @@
+const students = [
+  {text: 'Nick Manning', doneWithUnit1: true, _id: 123456},
+  {text: 'Student 2', doneWithUnit1: false, _id: 654321},
+  {text: 'Student 3', doneWithUnit1: false, _id: 010101},
+]
+
+export { 
+	students
+}
