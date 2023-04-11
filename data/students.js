a79503bd442f1a78488b4346ae65926243d5464a
Nicholas Manning <njmanning212@gmail.com>
2023-04-10 20:05:47 -0500

    dynamically create student list in /students with ejs including ids

--- data/students.js
+++ data/students.js
@@ -1,7 +1,7 @@
 const students = [
-  {text: 'Nick Manning', doneWithUnit1: true, _id: 123456},
-  {text: 'Student 2', doneWithUnit1: false, _id: 654321},
-  {text: 'Student 3', doneWithUnit1: false, _id: 123654},
+  {name: 'Nick Manning', doneWithUnit1: true, _id: 123456},
+  {name: 'Student 2', doneWithUnit1: false, _id: 654321},
+  {name: 'Student 3', doneWithUnit1: false, _id: 123654},
 ]
 
 export { 
